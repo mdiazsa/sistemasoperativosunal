63cf18b1315ce31152cf73fc4a8396663e2f6401
Manuel Diaz <82003614+mdiazsa@users.noreply.github.com>
2021-10-26 16:38:34 -0500

    Update tarea_asincronia.py

--- procesos/tarea_asincronia.py
+++ procesos/tarea_asincronia.py
@@ -1,1 +1,22 @@
+import asyncio
 
+
+async def main():
+    print("Inicio función main")
+    task1 = asyncio.create_task(imprimir("1"))
+    task2 = asyncio.create_task(imprimir("2"))
+    task3 = asyncio.create_task(imprimir("3"))
+    
+    await  task3
+    
+    print("Terminó la función main")
+
+
+async def imprimir(texto):
+    for i in range(3):
+        print("Esto se ejecutó en el task #" + texto + "\n")
+        await asyncio.sleep(0.5)
+
+
+
+asyncio.run(main())
