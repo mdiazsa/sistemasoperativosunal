9a392ea6e16879423bac3bb723ed64fcc884564e
Manuel Diaz <82003614+mdiazsa@users.noreply.github.com>
2021-10-26 17:08:49 -0500

    Update tarea_archivo.py

--- procesos/tarea_archivo.py
+++ procesos/tarea_archivo.py
@@ -1,1 +1,30 @@
+def leer():
+    archivo = open("file.txt", "r")
+    print(archivo.read() + "\n")
+    archivo.close()
 
+    main()
+
+def escribir():
+    archivo = open("file.txt", "w")
+    texto = input("Escriba lo que desee introducir: ")
+    archivo.write(texto)
+    archivo.close()
+    archivo = open("file.txt", "r")
+    print(archivo.read())
+    archivo.close()
+
+    main()
+
+def main():
+    print("Elija entre las siguientes opciones: \n 1) Leer el archivo \n 2) Escribir en el archivo")
+    opcion = input()
+    if opcion == "1":
+        leer()
+    elif opcion == "2":
+        escribir()
+
+    else:
+        main()
+
+main()
